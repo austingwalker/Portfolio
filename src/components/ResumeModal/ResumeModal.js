dd378affa1b60b0454d899f688e9d73a0039504e
World Wide <agwalker249@gmail.com>
2019-03-19 13:17:57 -0500

    changed resume link

--- src/components/ResumeModal/ResumeModal.js
+++ src/components/ResumeModal/ResumeModal.js
@@ -29,7 +29,7 @@ class ResumeModal extends React.Component {
   render() {
     return (
       <div className="resumeBox">
-        <Button className="resumeButton" href="http://www.keepandshare.com/doc22/view.php?id=108885&da=y" target="_blank" rel='noreferrer noopener' ><i className="far fa-file fa-3x"></i><h5>Resume</h5></Button>
+        <Button className="resumeButton" href="https://docs.google.com/document/u/1/d/e/2PACX-1vRr-TpJg1esEcOklHJpNb8r7BFp8RaCDwu2nvIio76rTRXrTgbZYpe3RX4qFl4DeEd8F8Jr6jwd9NUd/pub" target="_blank" rel='noreferrer noopener' ><i className="far fa-file fa-3x"></i><h5>Resume</h5></Button>
       </div>
     );
   }
