c7ad173f2340622b9883f0c789a700e03162a64c
World Wide <agwalker249@gmail.com>
2019-05-08 16:45:18 -0500

    Fixed typo and edited app info

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -6,10 +6,10 @@ const Content = props => (
     <div className="card portfolioDisplay">
             <img className="card-img-top" src={props.image} alt="App-Pic"/>
         <div className="card-body">
-            <h5 className="card-title">{props.name}</h5>
+            <h5 className="card-title"><b>{props.name}</b></h5>
             <p className="card-text"><small><b>Description:</b> {props.description}</small></p>
             <p className="card-text"><small><b>Technologies Used:</b> {props.technologies}</small></p>
-            <p><small><u><a className="card-text" href={props.deployedUrl} target="_blank" rel='noreferrer noopener'>Visit Depolyed App Here</a></u></small></p>
+            <p><small><u><a className="card-text" href={props.deployedUrl} target="_blank" rel='noreferrer noopener'>Visit Deployed App Here</a></u></small></p>
             <p><small><u><a className="card-text" href={props.githubRepo} target="_blank" rel='noreferrer noopener'>GitHub Repo</a></u></small></p>
 
         </div>
