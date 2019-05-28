0dc8e96ee6cacfdf3912c38f98008f4a3898a72b
World Wide <agwalker249@gmail.com>
2019-05-28 14:09:03 -0500

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PortfolioContainer from "./pages/PortfolioContainer";
-import Resume from "./components/Resume";
+
 
 const App = () => (
 <Router>
     <div>
       <Switch>
       <Route exact path="/" component={PortfolioContainer} /> 
-      {/* <Route exact path="/resume" component={Resume} />  */}
       </Switch>
     </div>
 </Router>
