6e9b584215d73abf98e9403f0b903848b8afdc6b
World Wide <agwalker249@gmail.com>
2019-05-28 15:19:33 -0500

    Updates

---
+++ precache-manifest.82cf69ddc588bf1821cb32507f60cf4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "4543ef7b4d691d2a6c9a",
+    "url": "/Portfolio/static/js/main.4543ef7b.chunk.js"
+  },
+  {
+    "revision": "91ebf4e22b945600b358",
+    "url": "/Portfolio/static/js/2.91ebf4e2.chunk.js"
+  },
+  {
+    "revision": "4543ef7b4d691d2a6c9a",
+    "url": "/Portfolio/static/css/main.fcd62c4c.chunk.css"
+  },
+  {
+    "revision": "a47230b1e333aa74048e07e5de255971",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.e95c8bdbb5e4f156ee11ed399d410c54.js"
+  "/Portfolio/precache-manifest.82cf69ddc588bf1821cb32507f60cf4d.js"
 );
 
 workbox.clientsClaim();
