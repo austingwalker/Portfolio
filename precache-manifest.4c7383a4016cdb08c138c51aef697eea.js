66051743da18fa3a94ced2e5538028e40f7d715d
World Wide <agwalker249@gmail.com>
2019-12-17 17:35:12 -0600

    Updates

---
+++ precache-manifest.4c7383a4016cdb08c138c51aef697eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.591eb5be.js"
+  },
+  {
+    "revision": "67c3c4e114048465e246",
+    "url": "/Portfolio/static/js/main.67c3c4e1.chunk.js"
+  },
+  {
+    "revision": "ecaa83e7089a9d3fa3f1",
+    "url": "/Portfolio/static/js/2.ecaa83e7.chunk.js"
+  },
+  {
+    "revision": "67c3c4e114048465e246",
+    "url": "/Portfolio/static/css/main.9356c54e.chunk.css"
+  },
+  {
+    "revision": "2dcb9b115d8a21ae92d23b943e50220a",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.46ba0354ca0752862ffab4f0a27e0b80.js"
+  "/Portfolio/precache-manifest.4c7383a4016cdb08c138c51aef697eea.js"
 );
 
 workbox.clientsClaim();
